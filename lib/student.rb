4e3a865750bb72a29a8bfa91b506f4cc035f8319
ConsolattaChepchirchir <consochep97@gmail.com>
2023-05-25 13:16:55 +0300

    complete task

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+#First it should inherit from the user which has the first name and last name    
+    class Student < User
+        attr_reader :knowledge
+    
+        def initialize
+            #initialize an empty knowledge array @knowledge
+            @knowledge = []
+        end
+        # takes in an argument of a string of knowledge 
+        # and adds it to the student's knowledge array
 
-end
+        def learn(info)
+            @knowledge <<info
+            
+        end
+        # finally return that student's knowledge array
+
+        def knowledge
+            @knowledge
+        end
+    end
+
---
+++ lib/user.rb
@@ -0,0 +1,5 @@
+#contains the first and last nane which is inherited by the student and teacher
+class User
+    attr_accessor :first_name, :last_name
+    
+end
